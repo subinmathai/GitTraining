04e0f3c7a889cef590e05cd09d87c84cdd0788a0
ADMIN <ADMIN@DESKTOP-M9N7H3Q>
2023-10-04 22:57:27 -0800

    Static program

---
+++ src/com/pbsquratraining/Trainingprogram/array/FactorialStatic.java
@@ -0,0 +1,23 @@
+package com.pbsquratraining.Trainingprogram.array;
+
+public class FactorialStatic 
+{
+	
+		public static void factorial(int num)
+		{
+			int fact=1;
+			for(int i=1;i<=num;i++)
+			{
+				fact=fact*i;
+			}
+			System.out.println(fact);
+		}
+			public static void main(String args[])
+			{
+			FactorialStatic.factorial(4);
+			
+				
+			}
+	
+
+}
