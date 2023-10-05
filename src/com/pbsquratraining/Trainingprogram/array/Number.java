ca93960c81ba8c88bf35b366afb5bbe16c9a6ce1
ADMIN <ADMIN@DESKTOP-M9N7H3Q>
2023-10-04 20:21:16 -0800

    for number arithemetical

---
+++ src/com/pbsquratraining/Trainingprogram/array/Number.java
@@ -0,0 +1,13 @@
+package com.pbsquratraining.Trainingprogram.array;
+
+public class Number
+{
+	public static void main(String args[])
+	{
+	int a=50;
+	int b=25;
+	int sum= a+b;
+	System.out.println("sum is"+sum);
+	}
+
+}
---
+++ src/com/pbsquratraining/Trainingprogram/array/NumberSubtraction.java
@@ -0,0 +1,16 @@
+package com.pbsquratraining.Trainingprogram.array;
+
+public class NumberSubtraction 
+{
+		public static void main(String args[])
+		{
+		int a=50;
+		int b=25;
+		int sum= a+b;
+		System.out.println("sum is"+sum);
+		}
+
+	}
+
+
+
