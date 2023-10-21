cc023fee855c55a0c880380a92acf29705d8f8b6
ADMIN <ADMIN@DESKTOP-M9N7H3Q>
2023-10-20 19:58:32 -0800

    sum

---
+++ src/com/pbsquratraining/Trainingprogram/array/SumTwoNumbers.java
@@ -0,0 +1,21 @@
+package com.pbsquratraining.Trainingprogram.array;
+
+
+public class SumTwoNumbers
+{
+	public int  sum(int a, int b)
+	{
+	int sum= a+b;
+	return sum;
+	}
+	
+public static void main(String args[])
+{ 
+	SumTwoNumbers sn= new SumTwoNumbers();
+	sn.sum(21,30);
+	
+	//System.out.println("sum is ");
+	
+}
+}
+
