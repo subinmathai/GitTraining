071a925c53c081fb771817f2b41b2a6a78f8fed4
ADMIN <ADMIN@DESKTOP-M9N7H3Q>
2023-11-02 22:09:37 -0800

    car

---
+++ src/aggrigation/Car.java
@@ -0,0 +1,16 @@
+package aggrigation;
+
+public class Car 
+{
+	String color;
+	String carname;
+	int regno;
+	public Car(String color, String carname, int regno) {
+		super();
+		this.color = color;
+		this.carname = carname;
+		this.regno = regno;
+	}
+	
+
+}
