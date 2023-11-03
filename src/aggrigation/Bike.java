b725b7678861727d4b660094db7561493dd4ac9f
subinmathai <subifor@gmail.com>
2023-11-02 22:36:22 -0800

    my traing pjt

---
+++ src/aggrigation/Bike.java
@@ -0,0 +1,14 @@
+package aggrigation;
+
+public class Bike 
+{
+	String bikeName;
+	int Bregno;
+	public Bike(String bikeName, int bregno) {
+		super();
+		this.bikeName = bikeName;
+		Bregno = bregno;
+	}
+	
+
+}
---
+++ src/aggrigation/Company.java
@@ -0,0 +1,14 @@
+package aggrigation;
+
+public class Company 
+{
+	String companyname;
+	String location;
+	public Company(String companyname, String location) {
+		super();
+		this.companyname = companyname;
+		this.location = location;
+	}
+	
+
+}
